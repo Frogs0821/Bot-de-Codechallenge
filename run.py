39db2b21909f4ee3bdec9f77255f42c11fe471af
Gabriel Flores <eldalai@gmail.com>
2023-12-06 08:27:40 -0300

    basic random connect4 client

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import json
 from random import randint
 import sys
 import websockets
+import time
 
 
 async def send(websocket, action, data):
@@ -17,11 +18,14 @@ async def send(websocket, action, data):
 
 
 async def start(auth_token):
-    uri = "wss://4yyity02md.execute-api.us-east-1.amazonaws.com/ws?token={}".format(auth_token)
+    # uri = "ws://codechallenge-server-stg-50693b4b7f64.herokuapp.com/ws?token={}".format(auth_token)
+    # uri = "ws://codechallenge-server-f4118f8ea054.herokuapp.com/ws?token={}".format(auth_token)
+    uri = "ws://localhost:5000/ws?token={}".format(auth_token)
     while True:
         try:
             print('connection to {}'.format(uri))
             async with websockets.connect(uri) as websocket:
+                print('connection READY!')
                 await play(websocket)
         except KeyboardInterrupt:
             print('Exiting...')
@@ -61,41 +65,24 @@ async def play(websocket):
 
 
 async def process_your_turn(websocket, request_data):
-    if randint(0, 4) >= 1:
-        await process_move(websocket, request_data)
-    else:
-        await process_wall(websocket, request_data)
+    # if randint(0, 4) >= 1:
+    await process_move(websocket, request_data)
 
 
 async def process_move(websocket, request_data):
-        side = request_data['data']['side']
-        pawn_board = [[None for _ in range(9)] for _ in range(9)]
-        for row in range(9):
-            for col in range(9):
-                string_row = request_data['data']['board'][17*(row*2): 17*(row*2) + 17]
-                pawn_board[row][col] = string_row[col * 2]
-        for row in range(9):
-            for col in range(9):
-                if pawn_board[row][col] == side:
-                    from_row = row
-                    from_col = col
-                    to_col = col
-                    break
-        to_row = from_row + (1 if side == 'N' else -1)
-        if pawn_board[to_row][from_col] != ' ':
-            to_row = to_row + (1 if side == 'N' else -1)
-        await send(
-            websocket,
-            'move',
-            {
-                'game_id': request_data['data']['game_id'],
-                'turn_token': request_data['data']['turn_token'],
-                'from_row': from_row,
-                'from_col': from_col,
-                'to_row': to_row,
-                'to_col': to_col,
-            },
-        )
+    side = request_data['data']['side']
+    board = request_data['data']['board']
+    colums = board.find('|', 1) - 1
+    print(board)
+    await send(
+        websocket,
+        'move',
+        {
+            'game_id': request_data['data']['game_id'],
+            'turn_token': request_data['data']['turn_token'],
+            'col': randint(0, colums),
+        },
+    )
 
 
 async def process_wall(websocket, request_data):
